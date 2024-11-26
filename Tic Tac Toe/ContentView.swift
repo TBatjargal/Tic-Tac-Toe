d2d9a0b881a1a88a5f75fe05ec371dd11a42afd7
Tsolmon Batjargal <b.tsolmon0421@icloud.com>
2024-11-26 09:55:40 -0600

    Added X and O

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -17,27 +18,32 @@ struct ContentView: View {
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
                 ForEach(0..<9) { index in
                     ZStack {
-                    Color.blue
+                        Color.blue
                         Text(moves[index])
-                        .font(.system(size: 90))
-                        .fontWeight(.heavy)
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(130)
+                    .onTapGesture {
+                        withAnimation {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "0"
+                                xTurn.toggle()
+                            }
+                        }
+                    }
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
+                    ForEach(0..<9) { index in}
                 }
-                .frame(width: 120, height: 120, alignment: .center)
-                .cornerRadius(130)
-                .onTapGesture {
-                    moves[index] = "X"
-                }
+                .preferredColorScheme(.dark)
             }
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-                ForEach(0..<9) { index in}
-            }
-            .preferredColorScheme(.dark)
         }
     }
 }
